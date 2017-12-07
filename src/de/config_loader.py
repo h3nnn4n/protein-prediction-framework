c90834128ac6b9fca1ab1c414641c2361cb61b6a
h3nnn4n <uber.renan@gmail.com>
2017-12-07 11:16:42 -0200

    Fixed file naming with config

--- src/de/config_loader.py
+++ src/de/config_loader.py
@@ -6,7 +6,7 @@ class ConfigLoader:
         self.parameters = ['pname', 'pop_size', 'max_iters', 'c_rate', 'f_factor', 'allatom', 'coil_only', 'stage0_init',
                            'stage2_interval', 'stage2_all_interval', 'partial_reset', 'log_interval', 'island_interval', 'do_lhs',
                            'n_hashes', 'update_interval', 'change_interval', 'reset_d_trigger', 'reset_d_percent', 'cname']
-        self.defaults = ['1crn', 100, 50, 1.0, 0.5, False, False, False, -1, -1, -1, 10, 100, False, 10, 20, 100, 0.0, 0.75, '']
+        self.defaults = ['1crn', 100, 50, 1.0, 0.5, False, False, False, -1, -1, -1, 10, 100, False, 10, 20, 100, 0.0, 0.75, conf_file]
         self.p_values = []
 
         try:
@@ -19,7 +19,6 @@ class ConfigLoader:
                             self.p_values.append(config[k])
                         else:
                             self.p_values.append(self.defaults[n])
-                    self.p_values.append(conf_file.split('.')[0])
                 except yaml.YAMLError as ee:
                     print(ee)
         except Exception as e:
