8fa33e3deb582aa8a4b610a7b79c8fb4b02b147a
h3nnn4n <uber.renan@gmail.com>
2018-03-26 15:30:57 -0300

    Added a simple script to make tables from the logs

---
+++ src/de/table_maker.py
@@ -0,0 +1,58 @@
+import os
+import re
+
+
+names = os.listdir()
+
+targets = ['all_best.dat', 'all_rmsd.dat']
+
+data = {}
+keys = []
+
+ncols = 0
+
+for name in names:
+    if os.path.isdir(name) and len(name) == 4:
+        os.chdir(name)
+        for target in targets:
+            key = name + '_' + target.split('.')[0]
+
+            if key not in data.keys():
+                data[key] = []
+                keys.append(key)
+
+            with open(target) as f:
+                lines = f.readlines()
+
+                for line in lines:
+                    tokens = re.sub("\s+", " ", line.strip()).split(' ')
+
+                    if ncols == 0:
+                        ncols = len(tokens)
+
+                    data[key].append(tokens)
+        os.chdir('..')
+
+w = ncols - 2
+
+header = """\\begin{table}
+  \centering
+    \\begin{tabular}{l|""" + 'l|' * w + """l}
+      \hline"""
+
+tail = """  \end{tabular}
+  \caption{Comparison}
+  \label{result}
+\end{table}"""
+
+
+for key in keys:
+    print(header)
+    for d in data[key]:
+        for k, v in enumerate(d):
+            if k < len(d) - 1:
+                print(v, end=' & ')
+            else:
+                print(v, end=' \\\\ \hline\n')
+    print(tail)
+    print()
