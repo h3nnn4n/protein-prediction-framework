75fecb6669a528299632a29f09da0d15928a573f
h3nnn4n <uber.renan@gmail.com>
2018-06-21 18:39:22 -0300

    Barplot

--- src/de/metrics_stats.py
+++ src/de/metrics_stats.py
@@ -20,10 +20,6 @@ def corr(x, y, **kwargs):
     ax = plt.gca()
     ax.annotate(label, xy=(0.2, 0.95), size=20, xycoords=ax.transAxes)
 
-import matplotlib as mpl
-mpl.use('Agg')
-import matplotlib.pyplot as plt
-
 
 with open('data.pickle', 'rb') as f:
     p = pickle.load(f)
@@ -31,6 +27,8 @@ with open('data.pickle', 'rb') as f:
 data, prots, tests, tests_prots = p
 
 metrics = ['scorefxn', 'gdt_ts', 'gdt_ha', 'tm_score', 'maxsub', 'rmsd']
+metrics = ['gdt_ts', 'gdt_ha', 'tm_score', 'maxsub', 'rmsd']
+metrics = ['gdt_ts', 'gdt_ha', 'tm_score', 'maxsub']
 modes = ['before', 'after']
 
 general = {}
@@ -59,8 +57,8 @@ for prot in data:
                 general[mode][prot][test_name]['gdt_ha'].append(run['gdt_ha'][0])
                 general[mode][prot][test_name]['maxsub'].append(run['maxsub'])
                 general[mode][prot][test_name]['tm_score'].append(run['tm_score'])
-                general[mode][prot][test_name]['rmsd'].append(run['rmsd'])
-                general[mode][prot][test_name]['scorefxn'].append(run['scorefxn'])
+                #general[mode][prot][test_name]['rmsd'].append(run['rmsd'])
+                #general[mode][prot][test_name]['scorefxn'].append(run['scorefxn'])
 
 tm = []
 rmsd = []
@@ -70,35 +68,86 @@ gdt_ha = []
 scorefxn = []
 labels = []
 
-target_protein = '1zdd'
+values = {}
+
+target_protein = '1enh'
 
 for mode in modes:
     if mode != 'before':
         continue
     for p in prots:
+        if target_protein != p:
+            continue
         # print()
         for t in tests:
-            # print(t)
+            values[t] = []
+            print()
+            print(t)
             for m in metrics:
-                # print(t)
-                if t != 'sade_remc':
-                    continue
 
-                if p == target_protein and m == 'tm_score':
-                    tm.append(general[mode][p][t][m])
-                    labels.append(t)
-                if p == target_protein and m == 'rmsd':
-                    rmsd.append(general[mode][p][t][m])
-                if p == target_protein and m == 'gdt_ha':
-                    gdt_ha.append(general[mode][p][t][m])
-                if p == target_protein and m == 'gdt_ts':
-                    gdt_ts.append(general[mode][p][t][m])
-                if p == target_protein and m == 'maxsub':
-                    maxsub.append(general[mode][p][t][m])
-                if p == target_protein and m == 'scorefxn':
-                    scorefxn.append(general[mode][p][t][m])
+                #values[t].append(general[mode][p][t][m])
+                v = (general[mode][p][t][m])
+
+                v = np.mean(v)
+                #if m in ['scorefxn', 'rmsd']:
+                    #v = min(v)
+                #else:
+                    #v = max(v)
+
+                print(v)
+                values[t].append(v)
+
+                # print(t)
+                #if t != 'sade_remc':
+                    #continue
+
+                #if p == target_protein and m == 'tm_score':
+                    #tm.append(general[mode][p][t][m])
+                    #labels.append(t)
+                #if p == target_protein and m == 'rmsd':
+                    #rmsd.append(general[mode][p][t][m])
+                #if p == target_protein and m == 'gdt_ha':
+                    #gdt_ha.append(general[mode][p][t][m])
+                #if p == target_protein and m == 'gdt_ts':
+                    #gdt_ts.append(general[mode][p][t][m])
+                #if p == target_protein and m == 'maxsub':
+                    #maxsub.append(general[mode][p][t][m])
+                #if p == target_protein and m == 'scorefxn':
+                    #scorefxn.append(general[mode][p][t][m])
         # print()
     # print()
+
+#n_groups = 8
+n_groups = len(metrics)
+
+fig, ax = plt.subplots()
+
+index = np.arange(n_groups)
+bar_width = 0.1
+
+opacity = 0.4
+error_config = {'ecolor': '0.3'}
+
+bars = []
+
+#print(n_groups, len(index), len(values[list(tests)[0]]))
+
+#print(values[list(tests)[0]])
+#plt.bar(index, values[list(tests)[0]])
+
+for k, t in enumerate(tests):
+#for m in metrics:
+    a = ax.bar(index + k * bar_width, values[t], bar_width,
+               alpha=opacity)
+
+    bars.append(a)
+    continue
+
+plt.savefig(target_protein + '_bars.png')
+
+#for k, v in values.items():
+    #print(k, v)
+    #print()
 
 # labels = list('ABCDEFGH')
 # fs = 10
@@ -129,23 +178,23 @@ for mode in modes:
 # plt.close()
 
 
-kappa = {
-            'scorefxn': scorefxn[0],
-            'gdt_ts': gdt_ts[0],
-            'gdt_ha': gdt_ha[0],
-            'tm_score': tm[0],
-            'maxsub': maxsub[0],
-            'rmsd': rmsd[0]
-        }
+#kappa = {
+            #'scorefxn': scorefxn[0],
+            #'gdt_ts': gdt_ts[0],
+            #'gdt_ha': gdt_ha[0],
+            #'tm_score': tm[0],
+            #'maxsub': maxsub[0],
+            #'rmsd': rmsd[0]
+        #}
 
-df = pd.DataFrame.from_dict(kappa, orient='columns')
+#df = pd.DataFrame.from_dict(kappa, orient='columns')
 
-grid = sns.PairGrid(data=df)
-#grid = grid.map_upper(plt.scatter)
-grid = grid.map_upper(sns.regplot)
-grid = grid.map_upper(corr)
-grid = grid.map_diag(plt.hist)
-#grid = grid.map_diag(sns.kdeplot)
-grid = grid.map_lower(sns.kdeplot)
+#grid = sns.PairGrid(data=df)
+##grid = grid.map_upper(plt.scatter)
+#grid = grid.map_upper(sns.regplot)
+#grid = grid.map_upper(corr)
+#grid = grid.map_diag(plt.hist)
+##grid = grid.map_diag(sns.kdeplot)
+#grid = grid.map_lower(sns.kdeplot)
 
-plt.savefig(target_protein + '_pairplot.png')
+#plt.savefig(target_protein + '_pairplot.png')
