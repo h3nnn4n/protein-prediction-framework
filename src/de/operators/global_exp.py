7443896fed7c0548f02539130ffc04e039a7e280
h3nnn4n <uber.renan@gmail.com>
2019-01-06 10:16:28 -0200

    Rewrite currToBest_exp_global and currToRand_exp_global to use base exp operator

--- src/de/operators/global_exp.py
+++ src/de/operators/global_exp.py
@@ -74,69 +74,16 @@ def best2exp_global(self, target):
 
 
 def currToRand_exp_global(self, target):
-    if not self.de.sade_run:
-        sade_k = 0
-    else:
-        sade_k = self.de.sade_ops.index(self.currToRand_exp_global)
-    self.de.sade_k = sade_k
-
-    cutPoint = random.randint(0, self.de.pop[0].nsca)
-    t_angle = []
-    ind1 = self.get_current_individual(target)
-    ind2, ind3 = self.get_random_individuals(n=2)
-    f, cr = self.de.get_f_cr()
-    L, r, pivot = 0, 0.0, cutPoint
-
-    for i in range(0, ind1.nsca):
-        t_angle.append(self.de.pop[target].angles[i])
-
-    while L < ind1.nsca and r < cr:
-            t_angle[pivot % ind1.nsca] = (
-                ind1.angles[pivot % ind1.nsca] +
-                (f * (ind2.angles[pivot % ind1.nsca] - ind3.angles[pivot % ind1.nsca]))
-            )
-
-            r = random.random()
-            L += 1
-            pivot += 1
-
-    self.de.trial.new_angles(t_angle)
-    self.de.trial.fix_bounds()
-    self.de.trial.eval()
-
-    self.de.selection(self.de.pop[target])
+    individuals = [self.get_current_individual(target)]
+    individuals.extend(self.get_random_individuals(n=2))
+    exp_base_operator(self, individuals, target, self.currToRand_exp_global)
 
 
 def currToBest_exp_global(self, target):
-    if not self.de.sade_run:
-        sade_k = 0
-    else:
-        sade_k = self.de.sade_ops.index(self.currToBest_exp_global)
-    self.de.sade_k = sade_k
-
-    cutPoint = random.randint(0, self.de.pop[0].nsca)
-    t_angle = []
-    ind1 = self.get_current_individual(target)
-    ind2 = self.get_best_individual()
-    ind3 = self.get_random_individual()
-    f, cr = self.de.get_f_cr()
-    L, r, pivot = 0, 0.0, cutPoint
-
-    for i in range(0, ind1.nsca):
-        t_angle.append(self.de.pop[target].angles[i])
-
-    while L < ind1.nsca and r < cr:
-            t_angle[pivot % ind1.nsca] = (
-                ind1.angles[pivot % ind1.nsca] +
-                (f * (ind2.angles[pivot % ind1.nsca] - ind3.angles[pivot % ind1.nsca]))
-            )
-
-            r = random.random()
-            L += 1
-            pivot += 1
-
-    self.de.trial.new_angles(t_angle)
-    self.de.trial.fix_bounds()
-    self.de.trial.eval()
-
-    self.de.selection(self.de.pop[target])
+    individuals = [
+        self.get_current_individual(target),
+        self.get_best_individual(),
+        self.get_random_individual()
+    ]
+    # individuals.extend(self.get_random_individuals(n=2))
+    exp_base_operator(self, individuals, target, self.currToRand_exp_global)
