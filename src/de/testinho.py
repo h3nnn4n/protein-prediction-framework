1f70387f6424a128d66c15951172d54d1ac59064
h3nnn4n <uber.renan@gmail.com>
2019-01-06 19:48:03 -0200

    Just testing around

--- src/de/testinho.py
+++ src/de/testinho.py
@@ -3,21 +3,20 @@ import pyrosetta
 
 target = 'MTKQEKTALNMARFIRSQTLTLLEKLNELDADEQADICESLHDHADELYRSCLARFGDDGENL'
 temp = 1.0
-n_moves = 1
-
+n_moves = 5
 
 pyrosetta.init('-out:level 0')
 
 scorefxn = pyrosetta.get_fa_scorefxn()
 pose = pyrosetta.pose_from_sequence(target)
-mc = pyrosetta.MonteCarlo(pose, scorefxn, 2.0)
+mc = pyrosetta.MonteCarlo(pose, scorefxn, temp)
 
 movemap = pyrosetta.MoveMap()
 movemap.set_bb(True)
 
 smallmover = pyrosetta.rosetta.protocols.simple_moves.SmallMover(movemap, temp, n_moves)
 shearmover = pyrosetta.rosetta.protocols.simple_moves.ShearMover(movemap, temp, n_moves)
-minmover = pyrosetta.rosetta.protocols.simple_moves.MinMover()
+minmover = pyrosetta.rosetta.protocols.minimization_packing.MinMover()
 
 perturbation_seq = pyrosetta.rosetta.protocols.moves.SequenceMover()
 perturbation_seq.add_mover(smallmover)
@@ -31,4 +30,8 @@ hopper.add_mover(minmover)
 hopper_trial = pyrosetta.TrialMover(hopper, mc)
 hopper_rep = pyrosetta.rosetta.protocols.moves.RepeatMover(perturbation_seq, 100)
 
-hopper_rep.apply(pose)
+
+def run(n=100):
+    for i in range(n):
+        hopper_rep.apply(pose)
+        print("%8d %12.4f" % (i, scorefxn(pose)))
