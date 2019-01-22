41205afe48898ea266fd958e66a9f1ef1c4b27bc
h3nnn4n <uber.renan@gmail.com>
2019-01-22 20:11:08 -0200

    Make magic.sh runnable on dumb osx

--- src/de/magic.sh
+++ src/de/magic.sh
@@ -11,5 +11,6 @@ re='s/base/'$a'/g'
 for myfile in base*.yaml; do
     target=$(echo $myfile|sed -e $re)
     cp -v "$myfile" "$target"
-    sed -i "$re" $target
+    sed -i .odeio_qndo_macos_foge_do_padrao_com_os_commandos_dos_nix_da_vida "$re" $target
+    rm *.odeio_qndo_macos_foge_do_padrao_com_os_commandos_dos_nix_da_vida
 done
