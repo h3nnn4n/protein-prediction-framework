f9b8ef8d88ad1b5a640b3d0d179ba90b0de215dd
h3nnn4n <uber.renan@gmail.com>
2019-01-22 21:27:32 -0200

    Save number of iterations and function evaluations

--- src/de/scipy_optmize_test.py
+++ src/de/scipy_optmize_test.py
@@ -4,6 +4,9 @@ from protein_data import ProteinData
 from rosetta_pack import RosettaPack
 
 
+attributes_to_save = ['fun', 'nfev', 'nit']
+
+
 def run_minimize(n_runs=40):
     rp = RosettaPack(name='1zdd')
     pd = ProteinData(rp)
@@ -25,10 +28,23 @@ def run_minimize(n_runs=40):
             result = minimize(pd, x0, method=method, tol=tol, options={'maxiter': maxiter, 'disp': disp})
 
             if method not in results.keys():
-                results[method] = []
+                results[method] = {}
 
-            results[method].append(result.fun)
-            print(result.fun)
+            for attr in attributes_to_save:
+                if not hasattr(result, attr):
+                    continue
+
+                if attr not in results[method].keys():
+                    results[method][attr] = []
+
+                value = getattr(result, attr)
+
+                if method == 'Powell' and attr == 'fun':
+                    value = float(value)
+
+                results[method][attr].append(value)
+
+            print("%20s %8d %8.3f" % (method, result['nfev'], result['fun']))
 
     return results
 
@@ -58,20 +74,33 @@ def run_dual_annealing(results=None, pd=None, n_runs=40):
         result = dual_annealing(pd, bounds=bounds, x0=x0, maxiter=maxiter, maxfun=maxfun)
 
         if method not in results.keys():
-            results[method] = []
+            results[method] = {}
 
-        results[method].append(result.fun)
-        print(result.fun)
+        for attr in attributes_to_save:
+            if not hasattr(result, attr):
+                continue
+
+            if attr not in results[method].keys():
+                results[method][attr] = []
+
+            value = getattr(result, attr)
+
+            if method == 'Powell' and attr == 'fun':
+                value = float(value)
+
+            results[method][attr].append(value)
+
+        print("%20s %8d %8.3f" % (method, result['nfev'], result['fun']))
 
     return results
 
 
-data = run_others(n_runs=40)
+data = run_others(n_runs=2)
 
 with open('scipy_min_dual_annealing.pickle', 'wb') as f:
     pickle.dump(data, f)
 
-data = run_minimize(n_runs=40)
+data = run_minimize(n_runs=10)
 
 with open('scipy_min_test.pickle', 'wb') as f:
     pickle.dump(data, f)
