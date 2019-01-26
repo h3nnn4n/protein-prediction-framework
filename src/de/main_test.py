2ee8a3ef6d899f53a9acb6dfddb5c203a32256c3
h3nnn4n <uber.renan@gmail.com>
2019-01-26 12:18:53 -0200

    Test that the code runs and create the log files

---
+++ src/de/main_test.py
@@ -0,0 +1,27 @@
+import pytest
+import os
+from main import boot
+
+
+def test_run():
+    files_before = os.listdir()
+    boot(None)
+    files_after = os.listdir()
+
+    s = set(files_before)
+    new_files = [x for x in files_after if x not in s]
+
+    assert prefix_in_filelist('best_0', new_files)
+    assert prefix_in_filelist('best_repacked_', new_files)
+    assert prefix_in_filelist('ops__', new_files)
+    assert prefix_in_filelist('stats__', new_files)
+    assert prefix_in_filelist('repack__', new_files)
+    assert prefix_in_filelist('parameters__', new_files)
+
+
+def prefix_in_filelist(prefix, filelist):
+    for file in filelist:
+        if prefix in file:
+            return True
+
+    return False
