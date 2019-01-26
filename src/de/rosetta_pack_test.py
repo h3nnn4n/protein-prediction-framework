d7ca8b89f54d079fa07e4e9d050c83b3c410fd90
h3nnn4n <uber.renan@gmail.com>
2019-01-25 22:04:29 -0200

    Test that the correct sequence was loaded and that the frag sets arent empty

--- src/de/rosetta_pack_test.py
+++ src/de/rosetta_pack_test.py
@@ -17,3 +17,24 @@ def test_path_doesnt_change():
 
     assert original_path == after_path
     assert original_path == after_path2
+
+
+def test_fragset_loaded():
+    """
+        Test that the 3 and 9mers fragments were loaded
+    """
+
+    rp = RosettaPack(name=pname)
+
+    assert rp.fragset3.size() > 0
+    assert rp.fragset9.size() > 0
+
+
+def test_native_pose_loaded():
+    """
+        Test that pose was loaded with the native sequence
+    """
+
+    rp = RosettaPack(name=pname)
+
+    assert rp.native.sequence() == rp.target
