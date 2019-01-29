dc5cf1dc86cc52631fa2e3bad5d4e1ae0e019b7e
h3nnn4n <uber.renan@gmail.com>
2019-01-28 22:29:26 -0200

    Add dunn table generation

--- src/de/dunn.py
+++ src/de/dunn.py
@@ -4,6 +4,18 @@ import os
 import numpy as np
 from scikit_posthocs import posthoc_dunn
 
+w = 3
+
+header = """\\begin{table}
+  \centering
+    \\begin{tabular}{l|""" + 'l|' * w + """l}
+      \hline"""
+
+tail = """  \end{tabular}
+  \caption{%s}
+  \label{result}
+\end{table}"""
+
 
 def load_data(target):
     original_path = None
@@ -21,14 +33,16 @@ def load_data(target):
     return data
 
 
-def dunn(data, alpha=0.05, metric=None):
+def dunn(data, alpha=0.05, metric=None, target=''):
     dunn_result = posthoc_dunn(list(data.values()), p_adjust='sidak')
 
     methods = list(data.keys())
     n_methods = len(methods)
     names = [method[5:][:-8] for method in methods]
-    print("Comparing %s %s with %d runs each and alpha: %6.2f" % (metric, names, len(data[methods[0]]), alpha))
+    string = "Comparing %s for the protein %s for the methods %s with %d runs each and alpha = %f" % (metric, target.upper(), names, len(data[methods[0]]), alpha)
 
+    print()
+    print(header)
     for i in range(n_methods):
         method_a = methods[i]
         data_a = data[method_a]
@@ -43,9 +57,10 @@ def dunn(data, alpha=0.05, metric=None):
             if p < alpha / 2.0 and mean_a < mean_b:
                 name_a = method_a[5:][:-8]
                 name_b = method_b[5:][:-8]
-                print("%25s  %25s  p: %8.4f  mean_a: %6.2f  mean_b: %6.2f" % (
+                print("%25s & %25s & %8.4f & %6.2f & %6.2f \\\\ \\hline" % (
                     name_a, name_b, p, mean_a, mean_b
                 ))
+    print(tail % string)
 
 
 def process(target):
@@ -59,7 +74,7 @@ def process(target):
         for method in methods:
             metric_data[method] = data[method][metric]
 
-        dunn(metric_data, 0.05, metric)
+        dunn(metric_data, 0.05, metric, target)
         print()
 
 
@@ -70,5 +85,4 @@ if __name__ == "__main__":
         targets = sys.argv[1:]
 
     for target in targets:
-        print(target)
         process(target)
