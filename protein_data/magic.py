21d85cff89bc16b565305b86ca9a8ddae6f57b5c
h3nnn4n <uber.renan@gmail.com>
2019-01-30 00:04:02 -0200

    Support psipred

--- protein_data/magic.py
+++ protein_data/magic.py
@@ -2,9 +2,17 @@
 
 import subprocess
 import requests
+import shutil
 import gzip
 import sys
 import os
+
+
+do_psspred = False
+do_psipred = True
+
+psipred_path = '/home/h3nnn4n/progs/psipred'
+psipred_bin = 'runpsipred'
 
 args = sys.argv[1:]
 if len(args) < 1:
@@ -23,6 +31,7 @@ def fetch(target):
 
     os.makedirs('output')
     os.makedirs('psspred')
+    os.makedirs('psipred')
 
     pdbid = target.upper()
     url = "http://files.rcsb.org/download/%s.pdb.gz" % pdbid
@@ -42,25 +51,41 @@ def fetch(target):
         for chunk in r.iter_content(chunk_size=128):
             fd.write(chunk)
 
-    pss_path = "/home/h3nnn4n/PSSpred/PSSpred.pl"
+    if do_psspred:
+        pss_path = "/home/h3nnn4n/PSSpred/PSSpred.pl"
 
-    if not os.path.isfile(pss_path):
-        pss_path = "/home/h3nnn4n/rosetta/PSSpred/PSSpred.pl"
+        if not os.path.isfile(pss_path):
+            pss_path = "/home/h3nnn4n/rosetta/PSSpred/PSSpred.pl"
 
-    os.chdir('psspred')
+        os.chdir('psspred')
 
-    subprocess.call([pss_path, '../' + target + '.fasta'])
+        subprocess.call([pss_path, '../' + target + '.fasta'])
 
-    os.chdir("../")
+        os.chdir("../")
 
-    with open('psspred/seq.dat.ss', 'rt') as f_in, open(target + '.psipred.ss2', 'wt') as f_out:
-        c = 0
-        for line in f_in.readlines():
-            if c == 0:
-                c += 1
-                f_out.write('# PSIPRED VFORMAT (PSIPRED V2.6 by David Jones)\n')
-            else:
-                f_out.write(line)
+        with open('psspred/seq.dat.ss', 'rt') as f_in, open(target + '.psipred.ss2', 'wt') as f_out:
+            c = 0
+            for line in f_in.readlines():
+                if c == 0:
+                    c += 1
+                    f_out.write('# PSIPRED VFORMAT (PSIPRED V2.6 by David Jones)\n')
+                else:
+                    f_out.write(line)
+
+    if do_psipred:
+        original_path = os.getcwd()
+        os.chdir(psipred_path)
+
+        subprocess.call([
+            os.path.join(psipred_path, psipred_bin),
+            os.path.join(original_path, (target + '.fasta'))
+        ])
+
+        os.chdir(original_path)
+
+        src_file = os.path.join(psipred_path, target + '.ss2')
+        dest_file = os.path.join(original_path, target + '.psipred.ss2')
+        shutil.copyfile(src_file, dest_file)
 
     frag_path = '../../tools/frag_picker/frag_picker.sh'
 
