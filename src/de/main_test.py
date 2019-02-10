e91e6f9218377da2b3893c4c46343adf50fca65d
h3nnn4n <uber.renan@gmail.com>
2019-02-10 19:14:00 -0200

    Some Test coverage for lsh

--- src/de/main_test.py
+++ src/de/main_test.py
@@ -6,12 +6,12 @@ from main import boot, main
 
 mocked_config = {
     'pname': '1crn',
-    'pop_size': 10,
-    'max_evals': 500,
+    'pop_size': 50,
+    'max_evals': 1000,
     'stop_condition': 'evals',
     'stage0_init': 1,
     'sade_run': 1,
-    'sade_lp': 50,
+    'sade_lp': 10,
     'sade_selection': 'roulette',
     'enable_remc': 0,
     'ops': ['rand1bin_global',
@@ -67,6 +67,38 @@ def test_run_with_fake_file():
 
         s = set(files_before)
         new_files = [x for x in files_after if x not in s and '_test-run_'in x]
+
+        assert prefix_in_filelist('best_0', new_files)
+        assert prefix_in_filelist('best_repacked_', new_files)
+        assert prefix_in_filelist('ops__', new_files)
+        assert prefix_in_filelist('stats__', new_files)
+        assert prefix_in_filelist('repack__', new_files)
+        assert prefix_in_filelist('parameters__', new_files)
+
+        for file in new_files:
+            assert os.path.getsize(file) > 0
+
+        for file in new_files:
+            os.remove(file)
+
+
+def test_run_lsh_with_fake_file():
+    lsh_mocked_config = mocked_config.copy()
+    lsh_mocked_config['do_lsh'] = True
+    lsh_mocked_config['n_hashes'] = 2
+    lsh_mocked_config['n_buckets'] = 5
+    lsh_mocked_config['update_interval'] = 2
+    lsh_mocked_config['change_interval'] = 5
+    lsh_mocked_config['ops'] = ['rand1exp_lsh', 'monte_carlo_3']
+    cname = 'test-lsh-run'
+
+    with mock.patch("config_loader.ConfigLoader.get_config_file", mock.MagicMock(return_value=lsh_mocked_config)):
+        files_before = os.listdir()
+        boot(cname)
+        files_after = os.listdir()
+
+        s = set(files_before)
+        new_files = [x for x in files_after if x not in s and ('_%s_' % cname) in x]
 
         assert prefix_in_filelist('best_0', new_files)
         assert prefix_in_filelist('best_repacked_', new_files)
