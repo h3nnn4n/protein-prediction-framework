ae80cad147aad3c29e8a438a0eaeec3b8c3f6cb5
h3nnn4n <uber.renan@gmail.com>
2019-05-16 23:36:19 -0300

    Experiment with random rho

--- src/de/hooke_jeeves_playground.py
+++ src/de/hooke_jeeves_playground.py
@@ -3,6 +3,8 @@ import pytest
 import sys
 import os
 
+from random import random
+
 from rosetta_pack import RosettaPack
 from protein_data import ProteinData
 
@@ -14,8 +16,9 @@ rp = RosettaPack(name=pname)
 pd = ProteinData(rp)
 
 for i in range(10):
+    rho = random()
     score_before = pd(pd.angles)
-    evals, solution = hooke(pd, eps=1e-8, rho=0.9)
+    evals, solution = hooke(pd, eps=1e-4, rho=rho)
     score_after = pd(solution)
 
-    print('%3d %14.6f %14.6f %8d' % (i, score_before, score_after, evals))
+    print('%3d %14.6f %14.6f %8d %8.8f' % (i, score_before, score_after, evals, rho))
